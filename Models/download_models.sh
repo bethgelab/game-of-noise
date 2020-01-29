9cf6f77c0e61090c49458a8afd3235b704de6e11
Evgenia Rusak <34031285+EvgeniaAR@users.noreply.github.com>
2020-01-29 13:40:28 +0100

    specify download path for model download

--- Models/download_models.sh
+++ Models/download_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_SIN_Model.pth"
-wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_Model.pth"
-wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Speckle_Model.pth"
-wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_mult_Model.pth"
-wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_sigma_0.5_Model.pth"
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_SIN_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Speckle_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_mult_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_sigma_0.5_Model.pth" -P ./Models/
