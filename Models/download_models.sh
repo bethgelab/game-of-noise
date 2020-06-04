c5e3bc92bfc1cacf7b86e807111e779a8e2b8612
Roland Zimmermann <5895436+zimmerrol@users.noreply.github.com>
2020-06-04 17:17:49 +0200

    Update download_models.sh

--- Models/download_models.sh
+++ Models/download_models.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_SIN_Model.pth" -P ./Models/
 wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT3x3_SIN_Model.pth" -P ./Models/
+wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/ANT3x3_Model.pth" -P ./Models/
 wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Speckle_Model.pth" -P ./Models/
 wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_mult_Model.pth" -P ./Models/
 wget "https://github.com/bethgelab/game-of-noise/releases/download/v1.0/Gauss_sigma_0.5_Model.pth" -P ./Models/
